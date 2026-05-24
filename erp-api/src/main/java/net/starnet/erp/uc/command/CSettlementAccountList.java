e27e51f02ca2f6ea3311e5d70a73cdb2b7912cd3
19881502972 <2722319735@qq.com>
2026-05-24 10:42:57 +0800

    2026/05/24-10：42。1、添加注释。2、小幅度修改并完善代码内容。3、格式化部分文件，使其整洁明了。

--- erp-api/src/main/java/net/starnet/erp/uc/command/CSettlementAccountList.java
+++ erp-api/src/main/java/net/starnet/erp/uc/command/CSettlementAccountList.java
@@ -8,6 +8,7 @@ import net.starnet.erp.uc.model.SettlementAccount;
 import net.starnet.erp.uc.service.SettlementAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -29,6 +30,8 @@ public class CSettlementAccountList extends BaseCommand {
     @Override
     protected void doCommand() throws Exception {
         List<SettlementAccount> accountList = accountService.list();
+        // 按时间倒序：日期最新的账户排在最前面
+        accountList.sort((o1, o2) -> o2.getBalanceTime().compareTo(o1.getBalanceTime()));
 
         for (SettlementAccount account : accountList) {
             DictItem item = itemService.getById(account.getType());
